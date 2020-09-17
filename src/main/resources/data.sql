03562fdbeb18f9498474a19d96643afda038f945
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-17 01:35:50 -0400

    populating some data into person table in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ location varchar(255),
 birth_date timestamp,
 primary key(id)
 );
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10001, 'Awad', 'PA', sysdate())
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10002, 'Yasir', 'NY', sysdate())
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10003, 'Mohammed', 'NC', sysdate())
