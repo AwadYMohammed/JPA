bfb921b06f7e7c434fb35dc4f5d1ebdc1e9f00a4
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-17 04:58:42 -0400

    one to one relationship with jpa and hibernate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,28 @@
-create table person 
-(
-id integer not null,
-name varchar(255) not null,
-location varchar(255),
-birth_date timestamp,
-primary key(id)
-);
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10001, 'Awad', 'PA', sysdate())
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10002, 'Yasir', 'NY', sysdate())
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)VALUES(10003, 'Mohammed', 'NC', sysdate())
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10001,'JPA in 50 Steps', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10002,'Spring in 50 Steps', sysdate(), sysdate(),false);
+insert into course(id, name, created_date, last_updated_date,is_deleted) 
+values(10003,'Spring Boot in 100 Steps', sysdate(), sysdate(),false);
+
+
+insert into student(id,name,passport_id)
+values(20001,'Ranga',40001);
+insert into student(id,name,passport_id)
+values(20002,'Adam',40002);
+insert into student(id,name,passport_id)
+values(20003,'Jane',40003);
+
+insert into passport(id,number)
+values(40001,'E123456');
+insert into passport(id,number)
+values(40002,'N123457');
+insert into passport(id,number)
+values(40003,'L123890');
+
+insert into review(id,rating,description,course_id)
+values(50001,'FIVE', 'Great Course',10001);
+insert into review(id,rating,description,course_id)
+values(50002,'FOUR', 'Wonderful Course',10001);
+insert into review(id,rating,description,course_id)
+values(50003,'FIVE', 'Awesome Course',10003);
